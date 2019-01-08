3f41b1aadbe09871ce1ebbb806ad4de60a42b83e
Chiguripati <Gowtham.Chiguripati@McKesson.com>
2019-01-07 21:23:16 -0500

    initial commit

---
+++ demo-rest-services/src/main/java/com/demo/rest/wservices/demorestservices/DemoRestServicesApplication.java
@@ -0,0 +1,14 @@
+package com.demo.rest.wservices.demorestservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRestServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoRestServicesApplication.class, args);
+	}
+
+}
+
