f3c19241d2d5a15beaff4b28eb82ab7c6b3dcfa2
Chiguripati <Gowtham.Chiguripati@McKesson.com>
2019-01-07 22:05:26 -0500

    first service/controller added

--- demo-rest-services/src/main/java/com/demo/rest/wservices/DemoRestServicesApplication.java
+++ demo-rest-services/src/main/java/com/demo/rest/wservices/DemoRestServicesApplication.java
@@ -1,4 +1,4 @@
-package com.demo.rest.wservices.demorestservices;
+package com.demo.rest.wservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ demo-rest-services/src/main/java/com/demo/rest/wservices/controller/Memos.java
@@ -0,0 +1,13 @@
+package com.demo.rest.wservices.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Memos {
+
+	@GetMapping("/hello")
+	public String HelloWorld() {
+		return "Hello World!!";
+	}
+}
